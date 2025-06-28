f041a5dc93f7b4e6b59b3de411b9cca380f4815c
vergelli <federico_joelvergelli@hotmail.com>
2025-06-28 16:27:22 -0300

    feat: Integrate Actors module and update sensor configuration in TmySim

--- src/TmySim.jl
+++ src/TmySim.jl
@@ -2,17 +2,19 @@ module TmySim
 
     include("../config/Config.jl")
     include("Simulator.jl")
+    include("Actors.jl")
 
     using Rocket
 
     using .Config
     using .Simulator
+    using .Actors
 
     function run()
-        sensor = SensorConfig("temp", 60.0, 0.1, t -> 25 + sin(t))
-        src = make_sensor_source(sensor)# |> take(10)
-        subscribe!(src, x -> println("Valor: ", x))
-        sleep(10)
+        sensor = SensorConfig("temp", 100.0, 0.1, t -> 25 + sin(t))
+        src = make_sensor_source(sensor)
+        actor = Actors.CompletionActor{Float64}()
+        subscribe!(src, actor)
+        take!(actor.completion_channel)
     end
-
 end
